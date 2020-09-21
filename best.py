64bc5aee7037baf6cbe3147e9185b53947372e9b
maximinus <maximinus@gmail.com>
2020-09-21 11:59:28 +0100

    Started new list

--- best.py
+++ best.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import random
 import datetime
 
 
@@ -17,8 +18,20 @@ class Show:
             day = f'0{self.date[0]}'
         else:
             day = str(self.date[0])
-        text_date = f'{day}-{MONTHS[self.date[1] - 1]}-{self.date[2]}'
-        return f'{self.score} {text_date} {self.review}'
+        if self.date[1] < 10:
+            month = f'0{self.date[1]}'
+        else:
+            month = str(self.date[1])
+        sc_txt = str(self.score)
+        size = len(sc_txt)
+        if size == 3:
+            final = sc_txt
+        elif size == 2:
+            final = f' {sc_txt}'
+        else:
+            final = f'  {sc_txt}'
+        text_date = f'{day}-{month}-{self.date[2]}'
+        return f'{text_date} {final} {self.review}'
 
 
 def getScores():
@@ -40,9 +53,7 @@ def getScores():
             day = int(dates[0])
             month = int(dates[1])
             year = int(dates[2])
-            # only add 5's
-            print(float(score))
-            if float(score) > 4.6:
+            if float(score) >= 3.6:
                 shows.append(Show(score, [day, month, year], review))
         except:
             continue
@@ -51,6 +62,6 @@ def getScores():
 
 if __name__ == '__main__':
     scores = getScores()
-    print(len(scores))
+    random.shuffle(scores)
     for i in scores:
         print(i)
